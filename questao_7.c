a540a3f40a41f2b0a4bfd283d04ff438922c8eb6
David-ard92 <andrew12regjs@gmail.com>
2026-05-29 16:18:40 -0400

    Update questao_7.c

--- questao_7.c
+++ questao_7.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+void inverter(char *texto) {
+
+    int tamanho = 0;
+	 // Conta o tamanho da string
+    while (texto[tamanho] != '\0') {
+        tamanho++;
+    }
+	  // Índice do início e do fim
+    int inicio = 0;
+    int fim = tamanho - 1;
+	  // Troca as letras das extremidades
+    while (inicio < fim) {
+
+        char aux = texto[inicio];
+
+        texto[inicio] = texto[fim];
+        texto[fim] = aux;
+
+        inicio++;
+        fim--;
+    }
+}
+
+int main() {
+
+    char palavra[100];
+
+    printf("Digite uma palavra:\n");
+    scanf("%s", palavra);
+
+    inverter(palavra);
+
+    printf("Palavra invertida: %s\n", palavra);
+
+    return 0;
+}
