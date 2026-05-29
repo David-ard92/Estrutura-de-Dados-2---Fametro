5cd98970bf9749a58306c971f730ecce2cf46d08
David-ard92 <andrew12regjs@gmail.com>
2026-05-29 16:16:20 -0400

    Update questao_4.c

--- questao_4.c
+++ questao_4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+void multiplicarPorDois(int *numero) {
+
+    *numero = *numero + *numero;
+
+}
+
+int main() {
+
+    int valor;
+
+    printf("Digite um numero:\n");
+    scanf("%d", &valor);
+
+    multiplicarPorDois(&valor);
+
+    printf("Resultado: %d\n", valor);
+
+    return 0;
+}
