61666c0b7054eea06fb7b1b063469e77e5cfb555
David-ard92 <andrew12regjs@gmail.com>
2026-05-29 16:16:55 -0400

    Update questao_5.c

--- questao_5.c
+++ questao_5.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+void copiar(char *texto1, char *texto2) {
+
+    int i = 0;
+
+    while (texto1[i] != '\0') {
+        texto2[i] = texto1[i];
+        i++;
+    }
+
+    texto2[i] = '\0';
+}
+
+int main() {
+
+    char palavra1[100];
+    char palavra2[100];
+
+    printf("Digite uma palavra:\n");
+    scanf("%s", palavra1);
+
+    copiar(palavra1, palavra2);
+
+    printf("Original: %s\n", palavra1);
+    printf("Copia: %s\n", palavra2);
+
+    return 0;
+}
