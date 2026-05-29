badef4fbddb31aaa1f7b1b141f8d463f2988d7a5
David-ard92 <andrew12regjs@gmail.com>
2026-05-29 16:13:40 -0400

    Create questao_1.c

---
+++ questao_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void ponteiros(int *n1, int *n2){
+
+    *n1 = 5;
+    *n2 = 10;    
+}
+
+int main (){
+    int n1, n2;
+  
+    printf ("Digite dois numeros:\n");
+    scanf ("%d %d", &n1, &n2);
+    
+    ponteiros (&n1, &n2);
+    
+    printf ("Os números via ponteiro são %d e %d", n1, n2);
+
+return 0;
+}
