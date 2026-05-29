908c831ea01535244f02dfaf87a7281fc8bc4bb0
David-ard92 <andrew12regjs@gmail.com>
2026-05-29 16:15:33 -0400

    Update questao_3.c

--- questao_3.c
+++ questao_3.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+
+    int numero;
+    int *ponteiro;
+
+    printf("Digite um numero:\n");
+    scanf("%d", &numero);
+
+    ponteiro = &numero;
+
+    printf("Numero digitado: %d\n", numero);
+    printf("Endereco da variavel: %p\n", &numero);
+    printf("Endereco guardado no ponteiro: %p\n", ponteiro);
+    printf("Valor acessado pelo ponteiro: %d\n", *ponteiro);
+
+    return 0;
+}
